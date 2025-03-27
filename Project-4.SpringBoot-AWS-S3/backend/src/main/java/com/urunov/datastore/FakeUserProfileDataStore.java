f26641111805d3e46f339f57836d3935298c3987
nikhilreddz-ops <76496874+nikhilreddz-ops@users.noreply.github.com>
2025-03-27 00:36:53 -0400

    Update FakeUserProfileDataStore.java

--- Project-4.SpringBoot-AWS-S3/backend/src/main/java/com/urunov/datastore/FakeUserProfileDataStore.java
+++ Project-4.SpringBoot-AWS-S3/backend/src/main/java/com/urunov/datastore/FakeUserProfileDataStore.java
@@ -7,20 +7,15 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.UUID;
 
-/**
- * Created by:
- * User: hamdamboy
- * Project: IntelliJ IDEA
- * Github: @urunov
- */
+
 @Repository
 public class FakeUserProfileDataStore {
 
     private static final LinkedList<UserProfile> USER_PROFILES = new LinkedList<UserProfile>();
 
     static {
-        USER_PROFILES.add(new UserProfile(UUID.randomUUID().toString(), "urunov", null));
-        USER_PROFILES.add(new UserProfile(UUID.randomUUID().toString(), "hamdamboy", null));
+        USER_PROFILES.add(new UserProfile(UUID.randomUUID().toString(), "nikhil", null));
+        USER_PROFILES.add(new UserProfile(UUID.randomUUID().toString(), "reddy", null));
     }
 
     public List<UserProfile> getUserProfiles(){
