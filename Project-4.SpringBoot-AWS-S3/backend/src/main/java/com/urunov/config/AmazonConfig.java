f6589f2b2548635b4d61bb40ae0d47cc314186ee
nikhilreddz-ops <76496874+nikhilreddz-ops@users.noreply.github.com>
2025-03-27 00:35:16 -0400

    Update AmazonConfig.java

--- Project-4.SpringBoot-AWS-S3/backend/src/main/java/com/urunov/config/AmazonConfig.java
+++ Project-4.SpringBoot-AWS-S3/backend/src/main/java/com/urunov/config/AmazonConfig.java
@@ -9,12 +9,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-/**
- * Created by:
- * User: hamdamboy
- * Project: IntelliJ IDEA
- * Github: @urunov
- */
+
 @Configuration
 public class AmazonConfig {
 
