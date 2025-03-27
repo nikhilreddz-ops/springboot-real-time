7fd0128f6aa2f2a8d9f9ebd0840b457a0ec0b53f
nikhilreddz-ops <76496874+nikhilreddz-ops@users.noreply.github.com>
2025-03-27 00:38:51 -0400

    Update EmployeeRepository.java

--- Registration-FullStack-Springboot/src/main/java/pagination/sort/repository/EmployeeRepository.java
+++ Registration-FullStack-Springboot/src/main/java/pagination/sort/repository/EmployeeRepository.java
@@ -4,11 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pagination.sort.model.Employee;
 
-/**
- * @Author: apple
- * @created on 03/07/2020
- * @Project is PaginationSort
- */
+
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 }
