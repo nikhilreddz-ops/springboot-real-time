4fea56ee8b448ef410eaa713b18a6d5da4d22b43
nikhilreddz-ops <76496874+nikhilreddz-ops@users.noreply.github.com>
2025-03-27 00:42:22 -0400

    Update Message.java

--- Spring Boot Configuration/SringXMLConfig/src/main/java/spring/configure/model/Message.java
+++ Spring Boot Configuration/SringXMLConfig/src/main/java/spring/configure/model/Message.java
@@ -1,10 +1,6 @@
 package spring.configure.model;
 
-/**
- * @Created 30 / 03 / 2020 - 10:13 AM
- * @project SpringXMLCongif
- * @Author Hamdamboy
- */
+
 public class Message {
     private int id;
     private String message;
